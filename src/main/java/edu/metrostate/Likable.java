c4ce1006cbd52f362f9ac776932d72834a751ca2
PaulTrevizo <paultrevizo@yahoo.com>
2024-09-21 14:36:01 -0500

    Likable.java
    Correction to code.

--- src/main/java/edu/metrostate/Likable.java
+++ src/main/java/edu/metrostate/Likable.java
@@ -1,6 +1,8 @@
 package edu.metrostate;
 
 public interface Likable {
-    public void addLike();
-    public void removeLike();
+    //"public" declaration is not needed as methods in interfaces are understood to be public
+    
+    void addLike();
+    void removeLike();
 }
