9278f6f0174c8c76bc6b431aad35fb7355cbe6d8
Chang Vang <Changvang32@gmail.com>
2024-09-29 20:09:14 -0500

    JavaFX - Controller + HomePage.fxml
    Added HomePage.fxml and its Controller

--- src/main/java/JavaFx/src/App.java
+++ src/main/java/JavaFx/src/App.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -9,29 +13,23 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+       
+  
+        Parent root;
+        try{
+            root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+            Scene scene = new Scene(root);
+  
+    
+  
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e){
+        }
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-  
-  /*
-  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-  Scene scene = new Scene(root);
-  */
-  
-  Scene scene = new Scene(root, 300, 250);
-  
-  primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        
     }
  
  public static void main(String[] args) {
---
+++ src/main/java/JavaFx/src/HomePageController.java
@@ -0,0 +1,29 @@
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class HomePageController {
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private ImageView image2;
+
+    @FXML
+    private ImageView image3;
+
+    @FXML
+    private ImageView image4;
+
+    @FXML
+    private ImageView image5;
+
+    @FXML
+    private TextField searchBar;
+
+}
