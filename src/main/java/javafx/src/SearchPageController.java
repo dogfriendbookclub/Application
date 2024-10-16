dafcb18e9531c852fde9b2153cd4ce7fa32b74d0
Chang Vang <Changvang32@gmail.com>
2024-10-15 20:19:03 -0500

    Search function testing
    Search function testing

--- src/main/java/javafx/src/SearchPageController.java
+++ src/main/java/javafx/src/SearchPageController.java
@@ -1,16 +1,24 @@
 package javafx.src;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonParser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class SearchPageController {
+public class SearchPageController implements Initializable{
 
     @FXML
     private Button homeButton;
@@ -19,13 +27,42 @@ public class SearchPageController {
     private AnchorPane rootPane;
 
     @FXML
-    private TextField searchBar;
-
-    @FXML
     void loadHomePage(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/homepage/HomePage.fxml"));
         AnchorPane pane = loader.load();
         rootPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Label searchResult;
+
+    public static Label static_label;
+
+   @FXML
+    private TextField txtfield;
+
+    @FXML
+    void loadSearchPage(ActionEvent event) throws IOException {
+
+        static_label.setText(txtfield.getText());
+
+//        listView.getItems().clear();
+//        listView.getItems().addAll(searchList(searchBar.getText()));
+
+//        searchResult = searchBar.getText();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/searchpage/SearchPage.fxml"));
+
+        AnchorPane pane = loader.load();
+        rootPane.getChildren().setAll(pane);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        static_label = searchResult;
     }
 
 }
