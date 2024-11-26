9599587986dc78dde788c7629d4f4d6f5798ffb3
Nicholas <ml2643yh@go.minnstate.edu>
2024-11-26 13:04:30 -0600

    display episode info

--- src/main/java/gui/episodeoverview/EpisodeOverviewController.java
+++ src/main/java/gui/episodeoverview/EpisodeOverviewController.java
@@ -2,18 +2,18 @@ package gui.episodeoverview;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import edu.metrostate.APIclient;
-import edu.metrostate.ShowPreview;
+import edu.metrostate.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 public class EpisodeOverviewController implements Initializable {
@@ -34,9 +34,24 @@ public class EpisodeOverviewController implements Initializable {
 
     @FXML
     private MenuItem seasonSelector;
-    
-    @FXML 
+
+    @FXML
     private MenuItem episodeSelector;
+
+    @FXML
+    private Label episodeNum;
+
+    @FXML
+    private ListView creatorList;
+
+    @FXML
+    private ListView castList;
+
+    @FXML
+    private Label episodeTitle;
+
+    @FXML
+    private TextArea synopsis;
 
     @FXML
     void loadSeasonPage(ActionEvent event) throws IOException {
@@ -47,7 +62,7 @@ public class EpisodeOverviewController implements Initializable {
     }
 
     @FXML
-    void loadEpisodePage(ActionEvent event) throws IOException{
+    void loadEpisodePage(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/episodeoverview/EpisodeOverview.fxml"));
 
 
@@ -61,7 +76,7 @@ public class EpisodeOverviewController implements Initializable {
         // returns a list of results (to the console atm) for the search query with the text from the search bar
         try {
             List<ShowPreview> testResults = apIclient.fetchSearchResults(searchBar.getText());
-            for ( ShowPreview result : testResults) {
+            for (ShowPreview result : testResults) {
                 System.out.println(result.toString());
             }
         } catch (Exception e) {
@@ -72,6 +87,22 @@ public class EpisodeOverviewController implements Initializable {
         AnchorPane pane = loader.load();
         rootPane.getChildren().setAll(pane);
     }
+    private Show show;
+
+    public void setShow(Show show) {
+        this.show = show;
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    public void setEpisode(Episode episode) {
+        this.episode = episode;
+    }
+
+    private Season season;
+    private Episode episode;
 
     /**
      * Called to initialize a controller after its root element has been
@@ -85,5 +116,26 @@ public class EpisodeOverviewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void populatecreators(Show show) {
+        List<Creator> creators = show.getCreators();  // Get the creators from the Show class
+        // Create a list to store the creator names
+        List<String> creatorNames = new ArrayList<>();
+        for (Creator creator : creators) {
+            creatorNames.add(creator.getName());  // Add creator name to the list
+        }
+        // Set the creator names to the ListView
+        ObservableList<String> observableCreatorNames = FXCollections.observableArrayList(creatorNames);
+        creatorList.setItems(observableCreatorNames);
+    }
+    public void loadEpisodeData(Show show, Season season, Episode episode) throws IOException {
+        populatecreators(show);
+        List<String> mainCast = apIclient.fetchCastForEpisode(season.getShowId(), season.getSeasonNumber(), episode.getEpisodeNum());
+        ObservableList<String> observableMainCastList = FXCollections.observableArrayList(mainCast);
+        castList.setItems(observableMainCastList);
+        episodeNum.setText(Integer.toString(episode.getEpisodeNum()));
+        synopsis.setText(episode.getSynopsis());
+        episodeTitle.setText(episode.getEpisodeName());
     }
 }
