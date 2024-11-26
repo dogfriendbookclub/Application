dfdc62ff6bd51a18fba001cf8644d50889bc4bc0
caLeb-centy <calebcenty18@gmail.com>
2024-11-26 07:09:37 -0600

    created ProfileController.java

--- src/main/java/gui/userprofile/ProfileController.java
+++ src/main/java/gui/userprofile/ProfileController.java
@@ -1,4 +1,55 @@
 package gui.userprofile;
 
-public class ProfileController {
+import edu.metrostate.User;
+import gui.content.ContentController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
+
+    @FXML
+    private ImageView profilePic;
+
+    @FXML
+    private Circle profileClip;
+
+    @FXML
+    private Text username;
+
+
+    private ProfileListener listener;
+
+
+    public interface ProfileListener{
+
+        User getUser();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Image image = new Image(getClass().getResource("/fxml/userprofile/rottenPotatoAvatar.png").toExternalForm());
+        profilePic.setImage(image);
+
+        // username.setText(listener.getUser().getUserHandle());
+        // System.out.println("set username on profile page to " + listener.getUser().getUserHandle());
+
+
+    }
+
+    public interface ProfilePageListener {
+        void showClickedOnInHome(int id);
+    }
+
+    public void setProfileListener(ProfileListener listener){
+        this.listener = listener;
+    }
 }
